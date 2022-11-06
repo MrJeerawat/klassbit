3c55c36d23182abf8f6a0f8c38c4602807ca5f25
MrJeerawat <jeerawat.taen@gmail.com>
2022-11-07 01:16:58 +0700

    add env

--- src/http/apiClient.js
+++ src/http/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const ApiCourse = axios.create({
-  baseURL: "http://localhost:8888/api/courses",
+  baseURL: process.env.REACT_APP_API_COURSES,
   headers: {
     "Content-type": "application/json"
   }
